6af27b1bbcc06156eb236824eb6dbf44e28d6f5e
Igor Brandalise <brandaliseigor123@gmail.com>
2025-08-01 12:53:16 -0300

    feat: classe com animação slide-down

--- src/components/telainicial.tsx
+++ src/components/telainicial.tsx
@@ -1,16 +1,17 @@
 import { FaPowerOff } from "react-icons/fa";
 import { Github, Linkedin } from "lucide-react";
+import { BatteryFull, Wifi } from "lucide-react";
 import { Separator } from "./separator";
 
 type Props = {
   voltarParaBoot: () => void;
-}
+};
 
-export function TelaInicial({voltarParaBoot}: Props) {
+export function TelaInicial({ voltarParaBoot }: Props) {
   return (
-    <div className="bg-windows-on">
-      <main>
-        <header className="flex justify-between items-center pt-12 p-17">
+    <div className="bg-windows-on ">
+      <main className="slide-down">
+        <header className="flex justify-between items-center pt-12 p-17 slide-down">
           <p className="text-zinc-400 font-bold text-[18px]">
             Made by{" "}
             <a
@@ -22,24 +23,29 @@ export function TelaInicial({voltarParaBoot}: Props) {
             </a>
           </p>
 
-          <button className="botao-boot-2 mr-20" onClick={voltarParaBoot}>
-            <FaPowerOff size={15} color="white" />
-          </button>
+          <div className="flex flex-row items-center gap-5 slide-down">
+            <button className="botao-boot-2 mr-5" onClick={voltarParaBoot}>
+              <FaPowerOff size={15} color="white" />
+            </button>
+
+            <BatteryFull size={25} color="white" />
+            <Wifi size={25} color="white" />
+          </div>
         </header>
 
-        <section className="flex flex-col items-center gap-8 mt-30">
+        <section className="flex flex-col items-center gap-8 mt-30 slide-down">
           <h1 className="text-[40px] text-zinc-50 font-semibold">Branda OS</h1>
 
-          <p className="text-black font-semibold text-[20px]">
+          <p className="text-[#A8A2A2]  text-[20px]">
             A little more about me.
           </p>
 
-          <button className="botao-power border-2 w-[13rem] rounded-4xl border-zinc-300 backdrop-blur-xl text-stone-50  p-3 text-[20px]">
+          <button className="botao-power border-2 w-[13rem] rounded-4xl border-zinc-300 backdrop-blur-xl text-stone-50  p-3 text-[20px] slide-down">
             Home Screen
           </button>
         </section>
 
-        <section className="flex  flex-row justify-center mt-22">
+        <section className="flex  flex-row justify-center mt-22 slide-down">
           <button className="botao-power justify-center flex items-center backdrop-blur-xl border-2 border-zinc-300 rounded-3xl p-3 w-[9rem] gap-5 hover:scale-100">
             <a
               href="https://github.com/Igor-Brandalise"
