4083e346de58bb38712b6d39db87dc3edac90d0a
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-07-31 22:17:43 -0300

    feat: função aoLigar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./index.css";
+import { TelaBoot } from "./components/telaboot";
+import { TelaInicial } from "./components/telainicial";
 
 function App() {
-  return (
-    <div className="bg-windows">
+  const [sistemaLigado, setSistemaLigado] = useState(false);
 
+  return (
+    <div className="">
+      {sistemaLigado ? (
+        <TelaInicial />
+      ) : (
+        <TelaBoot aoLigar={() => setSistemaLigado(true)} />
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
