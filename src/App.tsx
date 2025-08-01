b70d5c67bf45d478c68f37e5cd8cccf80f2e5c4c
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-07-31 22:26:53 -0300

    feat: adicionar lógica de transição entre telas boot e principal

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ import { TelaBoot } from "./components/telaboot";
 import { TelaInicial } from "./components/telainicial";
 
 function App() {
-  const [sistemaLigado, setSistemaLigado] = useState(false);
+  const [tela, setTela] = useState("boot");
 
-  return (
-    <div className="">
-      {sistemaLigado ? (
-        <TelaInicial />
-      ) : (
-        <TelaBoot aoLigar={() => setSistemaLigado(true)} />
-      )}
-    </div>
-  );
+  if (tela === "boot") {
+    return <TelaBoot aoLigar={() => setTela("principal")} />;
+  }
+
+  if (tela === "principal") {
+    return <TelaInicial />;
+  }
 }
 
 export default App;
