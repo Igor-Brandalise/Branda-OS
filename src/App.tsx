dde5c70b129ec59bcd803a719ca045e76dc8d0db
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-04 17:25:23 -0300

    feat: props da página about me

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { TelaBoot } from "./components/telaboot";
 import { TelaInicial } from "./components/telainicial";
 import { Desktop } from "./components/desktop";
+import { AboutMe } from "./components/aboutme";
 
 function App() {
   const [tela, setTela] = useState("boot");
@@ -12,12 +13,26 @@ function App() {
   }
 
   if (tela === "principal") {
-    return <TelaInicial voltarParaBoot={() => setTela("boot")} irParaDesktop={() => setTela("desktop")}/>;
+    return (
+      <TelaInicial
+        voltarParaBoot={() => setTela("boot")}
+        irParaDesktop={() => setTela("desktop")}
+      />
+    );
   }
 
-if (tela === "desktop") {
-  return <Desktop voltarParaBoot={() => setTela("boot")} />;
-}
+  if (tela === "desktop") {
+    return (
+      <Desktop
+        voltarParaBoot={() => setTela("boot")}
+        abrirAboutMe={() => setTela("aboutMe")}
+      />
+    );
+  }
+
+  if (tela === "aboutMe") {
+    return <AboutMe voltarParaDesktop={() => setTela("desktop")} voltarParaBoot={() => setTela("boot")}/>;
+  }
 
   return null;
 }
