bbe7b31140b52ccd578809a330f07a86b23bbfcc
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-04 15:45:39 -0300

    feat: página de desktop/props voltar para boot

---
+++ src/components/desktop.tsx
@@ -0,0 +1,27 @@
+import { BatteryFull, Wifi } from "lucide-react";
+import { DataHora } from "./data-hora.tsx";
+import { MenuLateral } from "./menu.tsx";
+
+type Props = {
+  voltarParaBoot: () => void;
+};
+
+export function Desktop({voltarParaBoot}: Props) {
+
+  return (
+    <div className="bg-windows-on">
+      <footer className="fixed bottom-0 w-full backdrop-blur-xl h-[4.7rem] text-white flex justify-between items-center px-4 py-2 shadow-inner select-none">
+        
+        <div>
+          <MenuLateral  voltarParaBoot={voltarParaBoot}/>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Wifi size={25} />
+          <BatteryFull size={25} />
+          <DataHora />
+        </div>
+      </footer>
+    </div>
+  );
+}
