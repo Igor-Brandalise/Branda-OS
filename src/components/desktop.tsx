13566a0686bc12b930ede83e2c748f4d79c7d8e4
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-04 22:53:05 -0300

    feat: passando as props para a página my recent work

--- src/components/desktop.tsx
+++ src/components/desktop.tsx
@@ -2,21 +2,24 @@ import { BatteryFull, Wifi } from "lucide-react";
 import { DataHora } from "./data-hora.tsx";
 import { MenuLateral } from "./menu.tsx";
 
-
 type Props = {
   voltarParaBoot: () => void;
   abrirAboutMe: () => void;
   topGames: () => void;
+  MyRecentWork: () => void;
 };
 
-export function Desktop({voltarParaBoot, abrirAboutMe, topGames}: Props) {
-
+export function Desktop({ voltarParaBoot, abrirAboutMe, topGames, MyRecentWork }: Props) {
   return (
     <div className="bg-windows-on">
       <footer className="fixed bottom-0 w-full backdrop-blur-xl h-[4.7rem] text-white flex justify-between items-center px-4 py-2 shadow-inner select-none">
-        
         <div>
-          <MenuLateral  voltarParaBoot={voltarParaBoot} AboutMe={abrirAboutMe} topGames={topGames}/>
+          <MenuLateral
+            voltarParaBoot={voltarParaBoot}
+            AboutMe={abrirAboutMe}
+            topGames={topGames}
+            MyRecentWork={MyRecentWork}
+          />
         </div>
 
         <div className="flex items-center gap-4">
